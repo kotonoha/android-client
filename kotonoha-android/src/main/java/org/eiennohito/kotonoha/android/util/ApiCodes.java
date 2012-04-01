481e74cab5a22ad6c0ac8a8b5ff5e6f2dd9ddc36
Arseny Tolmachev <eiennohito@gmail.com>
2012-04-01 13:13:03 +0600

    added api codes file

---
+++ kotonoha-android/src/main/java/org/eiennohito/kotonoha/android/util/ApiCodes.java
@@ -0,0 +1,10 @@
+package org.eiennohito.kotonoha.android.util;
+
+/**
+ * @author eiennohito
+ * @since 01.04.12
+ */
+public class ApiCodes {
+  public static final String publicKey;
+  public static final String privateKey;
+}
